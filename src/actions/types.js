7da4a4b5e922e8a68047e54886fb4c620fe35298
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-05-06 20:34:15 -0500

    Agregar loader a la aplicación

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,2 @@
 export const SET_POKEMONS = 'SET_POKEMOns'
+export const SET_LOADING = 'SET_LOADING'
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,13 +1,16 @@
-import { SET_POKEMONS } from "../actions/types";
+import { SET_LOADING, SET_POKEMONS } from "../actions/types";
 
 const initialState = {
     pokemons : [],
+    loading: false,
 };
 
 export const pokemonsReducer = (state = initialState, actions) => {
     switch(actions.type) {
         case SET_POKEMONS:
-            return {...state, pokemons: actions.payload };
+            return { ...state, pokemons: actions.payload };
+        case SET_LOADING:
+            return { ...state, loading: actions.payload }
     default:
         return state;
     }
